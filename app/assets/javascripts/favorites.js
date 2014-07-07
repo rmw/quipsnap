81eee016a7a6ebc7bc1972d3a14826ac34164493
Judy Jow <judy.jow@gmail.com>
2014-07-07 02:31:00 -0400

    User can see favorited quotes

--- app/assets/javascripts/favorites.js
+++ app/assets/javascripts/favorites.js
@@ -40,6 +40,58 @@ var quoteFavorites = {
 	}
 };
 
+var userFavorites = {
+	init: function() {
+		$(".nav-favorites").on("click", this.getFavoriteQuotes.bind(this));
+	},
+
+	getFavoriteQuotes: function(e) {
+		e.preventDefault();
+		var ajaxRequest = $.ajax({
+			url: "/favorites",
+			type: "get"
+		});
+
+		ajaxRequest.done(this.renderFavorites.bind(this));
+	},
+
+	renderFavorites: function(response) {
+		console.log(response);
+		$(".quotes").html("");	
+		for (var i = 0; i < response.quotes.length; i++) {
+			var html = this.getQuoteHtml(response.quotes[i]);
+			$(".quotes").append(html);
+		}
+	},
+
+	getQuoteHtml: function(quote) {
+		var html = "<div class='quote' id='" + quote.id + "'>" +
+			"<div class='content'>" + quote.content + "</div>" +
+				"<a class='search-author' data-method='post'" +
+				"href='/?q%5Bauthor_name_cont%5D=" + encodeURI(quote.author_name) + "' rel='nofollow'>" + quote.author_name + "</a>"
+		if (quote.book_title != "") {
+			html = html + "<a> , </a>" +
+				"<a class='search-title' data-method='post'" +
+				"href='/?q%5Bbook_title_cont%5D=" + encodeURI(quote.book_title) + 
+				"' rel='nofollow'>" + quote.book_title + "</a>";
+		}			
+
+		html = html + "<a class='search-user' data-method='post'" + 
+			"href='/?q%5Buser_goodreads_name_cont%5D="+ encodeURI(quote.user_name) + 
+			"' rel='nofollow'>Created by: " + quote.user_name + "</a>" + 
+			"<div class='quote-options'>" + 
+			"<button class='share-quote'>Share Quote</button>" + 
+			"<form action='/quotes/"+quote.id+"' class='button_to' method='get'><div>" +
+			"<input class='show-quote' type='submit' value='See more'></div></form>" +
+			"<button class='liked-quote'></button>" + 
+			"<button data-quote-id='" + quote.id + "' class='add-comment'>Add Comment</button></div></div>";
+
+		return html;
+	}
+
+};
+
 $(document).ready(function(){
 	quoteFavorites.init();
+	userFavorites.init();
 });
